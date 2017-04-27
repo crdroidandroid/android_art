c6cd48776b71c44ae9fa03701d184bcf159ecab3
Artem Serov <artem.serov@linaro.org>
2017-04-27 16:50:47 +0100

    ARM64: Share address computation across SIMD LDRs/STRs.
    For array accesses the element address has the following structure:
    Address = CONST_OFFSET + base_addr + index << ELEM_SHIFT
    
    Taking into account ARM64 LDR/STR addressing modes address part
    (CONST_OFFSET + index << ELEM_SHIFT) can be shared across array
    access with the same data type and index.
    
    For example, for the following loop 5 accesses can share address
    computation:
    
    void foo(int[] a, int[] b, int[] c) {
      for (i...) {
        a[i] = a[i] + 5;
        b[i] = b[i] + c[i];
      }
    }
    
    Test: test-art-host, test-art-target
    
    Change-Id: I46af3b4e4a55004336672cdba3296b7622d815ca
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- compiler/optimizing/nodes.h
+++ compiler/optimizing/nodes.h
@@ -1397,7 +1397,8 @@ class HLoopInformationOutwardIterator : public ValueObject {
   M(BitwiseNegatedRight, Instruction)                                   \
   M(DataProcWithShifterOp, Instruction)                                 \
   M(MultiplyAccumulate, Instruction)                                    \
-  M(IntermediateAddress, Instruction)
+  M(IntermediateAddress, Instruction)                                   \
+  M(IntermediateAddressIndex, Instruction)
 #endif
 
 #ifndef ART_ENABLE_CODEGEN_arm
