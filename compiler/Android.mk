multi-os-engine/art

Refactor SSA deconstruction into its own class
Test: m test-art-host

Change-Id: Ie82c2802f76f27512ef922ba583caeccf5675063

compiler/Android.mk
@@ -72,6 +72,7 @@ LIBART_COMPILER_SRC_FILES := \
 	optimizing/sharpening.cc \
 	optimizing/side_effects_analysis.cc \
 	optimizing/ssa_builder.cc \
+	optimizing/ssa_deconstruction.cc \
 	optimizing/ssa_liveness_analysis.cc \
 	optimizing/ssa_phi_elimination.cc \
 	optimizing/stack_map_stream.cc \

crdroidandroid/android_art

Signed-off-by: mydongistiny <jaysonedson@gmail.com>

compiler/Android.mk
@@ -68,6 +68,7 @@ LIBART_COMPILER_SRC_FILES := \
 	optimizing/prepare_for_register_allocation.cc \
 	optimizing/reference_type_propagation.cc \
 	optimizing/register_allocator.cc \
+	optimizing/register_allocation_resolver.cc \
 	optimizing/select_generator.cc \
 	optimizing/sharpening.cc \
 	optimizing/side_effects_analysis.cc \