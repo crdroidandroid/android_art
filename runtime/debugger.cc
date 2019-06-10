a742bda1e6a4f43dec14ba86ecd78e58eb2b6d4a
Andreas Gampe <agampe@google.com>
2019-06-10 11:01:43 -0700

    ART: Conditionally install Dbg class callbacks
    Temporary allocations in the dispatch code are unnecessary when they
    end up in disabled code. Instead follow the example of the method
    inspection callback and only add the callback when the debugger is
    actually active.
    
    Test: m test-art-host
    Test: atest CtsJdwpTestCases
    Change-Id: I750f2df475400147b1d569f463228d436aa3ae5a

--- runtime/debugger.cc
+++ runtime/debugger.cc
@@ -694,7 +694,8 @@ void Dbg::GoActive() {
   }
   instrumentation_events_ = 0;
   Runtime::DoAndMaybeSwitchInterpreter([=](){ gDebuggerActive = true; });
-  Runtime::Current()->GetRuntimeCallbacks()->AddMethodInspectionCallback(&gDebugActiveCallback);
+  runtime->GetRuntimeCallbacks()->AddClassLoadCallback(Dbg::GetClassLoadCallback());
+  runtime->GetRuntimeCallbacks()->AddMethodInspectionCallback(&gDebugActiveCallback);
   LOG(INFO) << "Debugger is active";
 }
 
@@ -732,7 +733,8 @@ void Dbg::Disconnected() {
         runtime->GetInstrumentation()->DisableDeoptimization(kDbgInstrumentationKey);
       }
       Runtime::DoAndMaybeSwitchInterpreter([=](){ gDebuggerActive = false; });
-      Runtime::Current()->GetRuntimeCallbacks()->RemoveMethodInspectionCallback(
+      runtime->GetRuntimeCallbacks()->RemoveClassLoadCallback(Dbg::GetClassLoadCallback());
+      runtime->GetRuntimeCallbacks()->RemoveMethodInspectionCallback(
           &gDebugActiveCallback);
     }
   }
