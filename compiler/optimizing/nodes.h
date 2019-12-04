00907667e7c4226c3887105a466d94cd6acd70e8
Artem Serov <artem.serov@linaro.org>
2019-12-04 21:02:09 +0000

    ART: Fix vectorizer HalvingAdd idiom
    IsAddConst2 function tried to extract addition chains
    for the halving add idiom: (A + B) >> 1. The problem
    was that regular shift right (x >> 1) was accepted for the
    idiom (with {A: x, B: 0}) and not processed as a shift - which
    broke the assumptions on shifts right and operand signedness.
    This CL fixes that.
    
    Test: 646-checker-simd-hadd.
    Test: test-art-target.
    
    xNombre: Removed references to nonexistent tests
    
    Change-Id: Icf71e1a8e8c54e68114d7d5d6c4aa8a47ea5234d
    Signed-off-by: xNombre <kartapolska@gmail.com>

--- compiler/optimizing/nodes.h
+++ compiler/optimizing/nodes.h
@@ -8116,6 +8116,10 @@ inline HInstruction* HuntForDeclaration(HInstruction* instruction) {
   return instruction;
 }
 
+inline bool IsAddOrSub(const HInstruction* instruction) {
+  return instruction->IsAdd() || instruction->IsSub();
+}
+
 void RemoveEnvironmentUses(HInstruction* instruction);
 bool HasEnvironmentUsedByOthers(HInstruction* instruction);
 void ResetEnvironmentInputRecords(HInstruction* instruction);
