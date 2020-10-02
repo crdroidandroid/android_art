7de8cd60ce56645c82307408380bbcb9b2183bdd
Vladimir Marko <vmarko@google.com>
2020-10-02 13:52:00 +0000

    Optimize JNI:GetStringRegion() for compressed strings.
    Looking at the disassembly, it seems clang++ was unable to
    optimize this properly, probably due to possible aliasing,
    despite inlining the String::CharAt() and seeing all the
    String::IsCompressed() expressions.
    
    Explicitly use the fact that the string is compressed to
    optimize the function and similar patterns in JNI.
    
    Test: m test-art-host-gtest
    Bug: 169674485
    Change-Id: I35dd2540a1e79611a65d2d84f2a557c826120409
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- runtime/jni/jni_internal.cc
+++ runtime/jni/jni_internal.cc
@@ -1972,8 +1972,9 @@ class JNI {
     } else {
       CHECK_NON_NULL_MEMCPY_ARGUMENT(length, buf);
       if (s->IsCompressed()) {
+        const uint8_t* src = s->GetValueCompressed() + start;
         for (int i = 0; i < length; ++i) {
-          buf[i] = static_cast<jchar>(s->CharAt(start+i));
+          buf[i] = static_cast<jchar>(src[i]);
         }
       } else {
         const jchar* chars = static_cast<jchar*>(s->GetValue());
@@ -1992,8 +1993,9 @@ class JNI {
     } else {
       CHECK_NON_NULL_MEMCPY_ARGUMENT(length, buf);
       if (s->IsCompressed()) {
+        const uint8_t* src = s->GetValueCompressed() + start;
         for (int i = 0; i < length; ++i) {
-          buf[i] = s->CharAt(start+i);
+          buf[i] = static_cast<jchar>(src[i]);
         }
       } else {
         const jchar* chars = s->GetValue();
@@ -2012,8 +2014,9 @@ class JNI {
       jchar* chars = new jchar[s->GetLength()];
       if (s->IsCompressed()) {
         int32_t length = s->GetLength();
+        const uint8_t* src = s->GetValueCompressed();
         for (int i = 0; i < length; ++i) {
-          chars[i] = s->CharAt(i);
+          chars[i] = static_cast<jchar>(src[i]);
         }
       } else {
         memcpy(chars, s->GetValue(), sizeof(jchar) * s->GetLength());
@@ -2059,9 +2062,10 @@ class JNI {
         *is_copy = JNI_TRUE;
       }
       int32_t length = s->GetLength();
+      const uint8_t* src = s->GetValueCompressed();
       jchar* chars = new jchar[length];
       for (int i = 0; i < length; ++i) {
-        chars[i] = s->CharAt(i);
+        chars[i] = static_cast<jchar>(src[i]);
       }
       return chars;
     } else {
@@ -2104,8 +2108,9 @@ class JNI {
     char* bytes = new char[byte_count + 1];
     CHECK(bytes != nullptr);  // bionic aborts anyway.
     if (s->IsCompressed()) {
+      const uint8_t* src = s->GetValueCompressed();
       for (size_t i = 0; i < byte_count; ++i) {
-        bytes[i] = s->CharAt(i);
+        bytes[i] = src[i];
       }
     } else {
       const uint16_t* chars = s->GetValue();
