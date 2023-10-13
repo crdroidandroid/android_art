757f9b670a41a7d7e926b678339f1acb4b3c746e
Santiago Aboy Solanes <solanes@google.com>
2023-10-13 14:21:30 +0100

    Add a new helper RecomputeDominatorTree
    It clears loop and dominance information, and builds the dominator
    tree. It also dchecks that we are not calling this methods with
    irreducible loops, as it is not supported.
    
    When adding this helper we found a partial LSE bug as it was
    recomputing dominator tree for irreducible loops.
    
    Test: art/test/testrunner/testrunner.py --host --64 -b --optimizing
    Bug: 304749506
    Change-Id: Ia4cc72cd19779ad881fa686e52b43679fe5a64d3

--- compiler/optimizing/load_store_elimination.cc
+++ compiler/optimizing/load_store_elimination.cc
@@ -1362,7 +1362,9 @@ class LSEVisitor final : private HGraphDelegateVisitor {
   }
 
   bool ShouldPerformPartialLSE() const {
-    return perform_partial_lse_ && !GetGraph()->IsCompilingOsr();
+    return perform_partial_lse_ &&
+           !GetGraph()->IsCompilingOsr() &&
+           !GetGraph()->HasIrreducibleLoops();
   }
 
   bool perform_partial_lse_;
@@ -3070,10 +3072,8 @@ class PartialLoadStoreEliminationHelper {
                                        Handle<mirror::DexCache>(),
                                        /* is_first_run= */ false);
     rtp_fixup.Visit(ArrayRef<HInstruction* const>(new_ref_phis_));
-    GetGraph()->ClearLoopInformation();
-    GetGraph()->ClearDominanceInformation();
     GetGraph()->ClearReachabilityInformation();
-    GetGraph()->BuildDominatorTree();
+    GetGraph()->RecomputeDominatorTree();
     GetGraph()->ComputeReachabilityInformation();
   }
 
