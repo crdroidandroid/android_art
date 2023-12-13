22bdffccf09efef41f5c73f640d221b1c79abf6f
Jiakai Zhang <jiakaiz@google.com>
2023-12-13 18:18:07 +0000

    Make profile inline cache threshold configurable.
    Before the change, the threshold was the same as the JIT threshold, which
    is 65535 by default.
    
    After the change, the threshold is unchanged but is configurable through
    a commandline flag "-Xps-inline-cache-threshold".
    
    Bug: 309434431
    Test: art/test.py -b --host -r -t 2271
    Test: m test-art-host-gtest-art_cmdline_tests
    Test: Set "dalvik.vm.ps-inline-cache-threshold" to different values, run
      a compose app benchmark, dump the profile, and see the difference in
      the profiles.
    Change-Id: I50a51cf7821b734b497d3a2e56261c5dfc10ef47

---
+++ test/2271-profile-inline-cache/run.py
@@ -0,0 +1,30 @@
+#
+# Copyright 2023 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+def run(ctx, args):
+  # Use
+  # --compiler-filter=verify to make sure that the test is not compiled AOT
+  # and to make sure the test is not compiled when loaded (by PathClassLoader)
+  # -Xjitsaveprofilinginfo to enable profile saving
+  # -Xjitinitialsize:32M to prevent profiling info creation failure.
+  ctx.default_run(
+      args,
+      Xcompiler_option=["--compiler-filter=verify"],
+      runtime_option=[
+          "-Xjitinitialsize:32M",
+          "-Xjitsaveprofilinginfo",
+          "-Xps-inline-cache-threshold:3000",
+      ])
